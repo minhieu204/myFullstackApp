2bc8faf9cceb02d90bb677201b0cf1ea8ac01456
minhieu204 <minhhieu13062004@gmail.com>
2025-08-11 17:39:47 +0700

    step 1

--- myReact/src/App.jsx
+++ myReact/src/App.jsx
@@ -1,8 +1,18 @@
+import axios from "./util/axios.customize"
+import { useEffect } from "react"
+
 function App() {
 
+  useEffect(() => {
+    const fetchHelloWorld = async() => {
+      const res = await axios.get(`/v1/api`);
+      console.log(res);
+    }
+    fetchHelloWorld()
+  }, [])
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>Hello world!</h1>
     </>
   )
 }
